24fa890006a06330b7bb24b7c323b968a94c2823
IgnacioPeralta00 <00196321@uca.edu.sv>
2024-11-14 23:54:26 -0600

    ⚡️ Added a ID atribute for Paciente class

--- main.cc
+++ main.cc
@@ -9,10 +9,11 @@ private:
     std::string nombre;
     std::string condicion;
     int sesiones_completadas;
+    int id;
 
 public:
-    Paciente(std::string _nombre, std::string _condicion, int sesiones) : // Constructor parametrizado
-                                                                          nombre(_nombre), condicion(_condicion), sesiones_completadas(sesiones)
+    Paciente(std::string _nombre, std::string _condicion, int sesiones, int _id) : // Constructor parametrizado
+                                                                                   nombre(_nombre), condicion(_condicion), sesiones_completadas(sesiones), id(_id)
     {
     }
     // Metodos getter
@@ -28,10 +29,18 @@ public:
     {
         return this->sesiones_completadas;
     }
+    int getID()
+    {
+        return this->id;
+    }
     // Metodos setter
     void setSesiones(int sesion)
     {
         this->sesiones_completadas = sesion;
+    }
+    void setID(int id) // Si se llega a necesitar un cambio de id, diferente al asignado inicialmente
+    {
+        this->id = id;
     }
 };
 
@@ -45,17 +54,20 @@ public:
     {
         return (rand() % 900 + 100);
     }
-    
+
     void AgregarPaciente()
     {
         std::string nombre, condicion;
+        int _id = 0;
         std::cout << "\n\t|NUEVO PACIENTE|" << std::endl;
         std::cout << "Nombre: ";
         std::getline(std::cin, nombre);
         std::cout << "Condicion: ";
         std::getline(std::cin, condicion);
+        _id = GeneradorID();
+        std::cout << "ID asignado: #" << _id;
 
-        lista_pacientes.push_back(Paciente(nombre, condicion, 0)); // Un nuevo paciente no posee sesiones completadas
+        lista_pacientes.push_back(Paciente(nombre, condicion, 0, _id)); // Un nuevo paciente no posee sesiones completadas
     }
 
     void MostrarPacientes()
@@ -67,6 +79,7 @@ public:
             std::cout << "Nombre: " << p.getNombre() << std::endl;
             std::cout << "Condicion: " << p.getCondicion() << std::endl;
             std::cout << "Sesiones completadas: " << p.getSesiones() << std::endl;
+            std::cout << "ID: #" << p.getID() << std::endl;
             std::cout << "-------------------------\n";
         }
     }
